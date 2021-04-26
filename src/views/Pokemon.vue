fc860d4b3a0520a0e5f441a5ec16f58a198d47fb
sergio9815 <gsergio1598@gmail.com>
2021-04-25 23:54:12 -0500

    :lipstick: Agregando tipos de pokemon

--- src/views/Pokemon.vue
+++ src/views/Pokemon.vue
@@ -1,5 +1,5 @@
 <template>
-  <p>Pokemon</p>
+  <p class="normal">Pokémon</p>
 </template>
 
 <script>
@@ -7,12 +7,43 @@ export default {
   data() {
     return {
       title: 'Pokémon | ',
+      types: [
+        { name: 'normal', icon: ' 🍥' },
+        { name: 'fire', icon: ' 🔥' },
+        { name: 'water', icon: ' 🌊' },
+        { name: 'plant', icon: ' 🌿' },
+        { name: 'electric', icon: ' ⚡' },
+        { name: 'ice', icon: ' ❄️' },
+        { name: 'fight', icon: ' 🥊' },
+        { name: 'ground', icon: ' 🟤' },
+        { name: 'flying', icon: ' 🦅' },
+        { name: 'psychic', icon: ' 🔮' },
+        { name: 'bug', icon: ' 🐛' },
+        { name: 'ghost', icon: ' 👻' },
+        { name: 'dragon', icon: ' 🐲' },
+        { name: 'dark', icon: ' 💀' },
+        { name: 'fairy', icon: ' 🎆' },
+        { name: 'steel', icon: ' 🔩' },
+        { name: 'rock', icon: ' 🔘' },
+      ],
       pokemon: '',
+      type: '',
     }
   },
   created() {
+    this.getType('ghost')
     this.pokemon = this.$route.params.id
-    document.title = this.title + this.pokemon
+    document.title = this.title + this.pokemon + this.type
+  },
+
+  methods: {
+    getType(name) {
+      this.types.forEach((type) => {
+        return type.name === name ? (this.type = type.icon) : ' '
+      })
+    },
   },
 }
 </script>
+
+<style scoped></style>
