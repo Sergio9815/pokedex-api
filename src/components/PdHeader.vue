492c316b4a1cf472cadeb1af43fc5c930ec4cfd8
sergio9815 <gsergio1598@gmail.com>
2021-05-06 22:07:16 -0500

    :sparkles: Slide

--- src/components/PdHeader.vue
+++ src/components/PdHeader.vue
@@ -2,6 +2,7 @@
   <nav class="container">
     <div class="container__menu">
       <a @click="showMenu" href=""><i class="fas fa-bars"></i></a>
+      <pd-slide />
     </div>
     <div class="container__title">
       <router-link class="container__link" :to="{ name: 'home' }">
@@ -14,8 +15,11 @@
 </template>
 
 <script>
+import PdSlide from '@/components/PdSlide'
+
 export default {
   name: 'PdHeader',
+  components: { PdSlide },
   data() {
     return {
       title: 'Poke',
---
+++ src/components/PdSlide.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="slide">
+    <div class="data">
+      <p>HOLA</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Slide',
+  data() {
+    return {
+      links: [],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.slide {
+  width: 10em;
+  height: 100vh;
+  background: black;
+  color: white;
+}
+</style>
