c8af6003c3e38d65533d79bc94554a699125a7b4
sergio9815 <gsergio1598@gmail.com>
2021-06-29 01:41:30 -0500

    :sparkles: New buttons: home, pokemons

--- src/components/PdButtons.vue
+++ src/components/PdButtons.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="container__buttons">
+    <a class="btn link" href=""
+      >HOME</a
+    >
+    <a class="btn link" href=""
+      >POKEMONS</a
+    >
     <a class="btn" href="https://github.com/Sergio9815/pokedex-api"
       ><i class="fab fa-github"></i>GITHUB</a
-    >
-    <a class="btn" href="https://www.instagram.com/sagb_24/"
-      ><i class="fab fa-instagram"></i>INSTAGRAM</a
     >
   </div>
 </template>
