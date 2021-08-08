a5743e875c1f75bff4042c7d2576090c09dea47c
Sergio9815 <gsergio1598@gmail.com>
2021-08-08 18:04:37 -0500

    :art: add new routes

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home'
 import Pokemon from '@/views/Pokemon'
+import AllPokemons from '@/views/AllPokemons'
 import Error from '@/views/NotFound'
 
 Vue.use(Router)
@@ -19,6 +20,11 @@ export default new Router({
       path: '/pokemon/:id',
       name: 'pokemon',
       component: Pokemon,
+    },
+    {
+      path: '/allpokemons',
+      name: 'allpokemons',
+      component: AllPokemons,
     },
     {
       path: '*',
